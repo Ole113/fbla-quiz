799dc7b5cfe53f70d063a4859f50775447923a72
Ole113 <alex_elbel@icloud.com>
2020-09-03 08:28:40 -0600

    Added documentation

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,16 +2,22 @@ import React from "react";
 import "../assets/css/Navbar.css";
 import { Link } from "react-router-dom";
 
+/*
+ * Renders the Navbar using the supplied props.
+ */
 export default function Navbar(props) {
     return (
         <nav className={`navbar fixed-top navbar-expand-sm ${props.active === "landing" ? window.innerWidth <= 576 ? "navbar-custom navbar-light bg-light" : "navbar-custom" : "navbar-light bg-light"}`}>
+            { /* The logo and main title of the Navbar */ }
             <a className="navbar-brand" href={props.links[0]}>
                 <img src={props.image} width="30" height="30" className="d-inline-block align-top" alt="" />
                 FBLA Quiz
             </a>
+            { /* The Navbar toggler when the screen size is mobile to see the Navbar */ }
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
+            { /* The Navbar elements that link to the specified pages */ }
             <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav">
                     <li className={`nav-item${props.active === "landing" ? " active-tab" : ""}`}>
