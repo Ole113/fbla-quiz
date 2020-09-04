328e2448712abcb862b4d742908fe8d90b1b18ab
Ole113 <alex_elbel@icloud.com>
2020-09-04 12:53:16 -0600

    Added documentation

--- server.js
+++ server.js
@@ -5,17 +5,18 @@ const CRON = require("node-cron");
 
 const APP = EXPRESS();
 
+//Schedules a CRON job every 3 hours(180 minutes) to backup the database to the backups folder.
 CRON.schedule("*/180 * * * *", () => {
-    console.log("Running task every 3 hours");
-    let exec = require('child_process').exec, child;
+    let exec = require("child_process").exec, child;
 
-    child = exec('node backup/backupScript.js', (error, stdout, stderr) => {
+    child = exec("node backup/backupScript.js", (error, stdout, stderr) => {
         console.log("stdout: " + stdout);
         console.log("stderr: " + stderr);
         if (error) console.log("Exec Error: " + error);
     });
 });
 
+//Sets up the connection info to the MySQL database.
 let connection = MYSQL.createConnection({
     host: "localhost",
     user: "root",
@@ -23,10 +24,12 @@ let connection = MYSQL.createConnection({
     database: "Questions"
 });
 
+//Connects to the MySQL database.
 connection.connect(err => { if (err) throw err; });
 
 APP.use(CORS());
 
+//Sets the URL directory "/questions" to return the JSON.
 APP.get("/questions", (req, res) => {
     connection.query("SELECT * from questions", function (err, result) {
         if (err) throw err;
@@ -34,6 +37,7 @@ APP.get("/questions", (req, res) => {
     });
 });
 
+//Creates the server on port 5000.
 APP.listen(5000, () => {
     console.log("Server successfully started.")
 });
