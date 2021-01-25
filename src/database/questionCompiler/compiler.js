677726eb0626135577df67af5f57a8edfa1a437b
Ole113 <alex_elbel@icloud.com>
2021-01-25 08:22:56 -0700

    Added const convention.

--- src/database/questionCompiler/compiler.js
+++ src/database/questionCompiler/compiler.js
@@ -1,7 +1,7 @@
 const READLINE = require("readline");
 const MYSQL = require("mysql");
 
-let configFileName = "config.json";
+const CONFIG_FILE_NAME = "config.json";
 
 const RL = READLINE.createInterface({
 	input: process.stdin,
@@ -9,24 +9,24 @@ const RL = READLINE.createInterface({
 });
 
 RL.question("Example config file name: \"config.json\". \nType \"exit\" to quit. \nLeave the input blank for the default file \"default.json\".\n\n  Config File: ", file => {
-	if (file !== "") configFileName = file;
+	if (file !== "") CONFIG_FILE_NAME = file;
 	if (file == "exit") RL.close();
-	configFile = require(`./${configFileName}`);
+	const CONFIG_FILE = require(`./${CONFIG_FILE_NAME}`);
 
-	queryQuestions(configFile)
+	queryQuestions(CONFIG_FILE)
 });
 
 RL.on("close", () => { process.exit(0); });
 
 function queryQuestions(res) {
-	let connection = MYSQL.createConnection({
+	const CONNECTION = MYSQL.createConnection({
 		host: res.host,
 		user: res.user,
 		password: res.password,
 		database: res.database
 	});
 
-	connection.connect(err => {
+	CONNECTION.connect(err => {
 		if (err) {
 			console.error("An error occurred, most likely the config file had incorrect values. " + err.stack);
 			return;
@@ -34,9 +34,9 @@ function queryQuestions(res) {
 	});
 
 	for (let i = 0; i < res.questions.length; i++) {
-		connection.query(`INSERT INTO questions (content, category, option_one, option_two, option_three, option_four) VALUES ("${res.questions[i][0]}", "${res.questions[i][1]}", "${res.questions[i][2][0]}", "${res.questions[i][2][1]}", "${res.questions[i][2][2]}", "${res.questions[i][2][3]}") `, error => {
+		CONNECTION.query(`INSERT INTO questions (content, category, option_one, option_two, option_three, option_four) VALUES ("${res.questions[i][0]}", "${res.questions[i][1]}", "${res.questions[i][2][0]}", "${res.questions[i][2][1]}", "${res.questions[i][2][2]}", "${res.questions[i][2][3]}") `, error => {
 			if (error) {
-				return connection.rollback(function () {
+				return CONNECTION.rollback(function () {
 					throw error;
 				});
 			}
